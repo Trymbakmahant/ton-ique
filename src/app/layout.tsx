fda5a28190974b930e63c34e02f8c75656fd82c1
Santhoshkumar <158072171+santhoshkumar0918@users.noreply.github.com>
2024-12-04 15:53:26 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,6 @@
-// import type { PropsWithChildren } from 'react';
-// import type { Metadata } from 'next';
-// import { getLocale } from 'next-intl/server';
-
-// import { Root } from '@/components/Root/Root';
-// import { I18nProvider } from '@/core/i18n/provider';
-
-// import '@telegram-apps/telegram-ui/dist/styles.css';
-// import 'normalize.css/normalize.css';
-// import './_assets/globals.css';
-
-// export const metadata: Metadata = {
-//   title: 'Your Application Title Goes Here',
-//   description: 'Your application description goes here',
-// };
-
-// export default async function RootLayout({ children }: PropsWithChildren) {
-//   const locale = await getLocale();
-
-//   return (
-//     <html lang={locale}>
-//     <body>
-//       <I18nProvider>
-//         <Root>
-//           {children}
-//         </Root>
-//       </I18nProvider>
-//     </body>
-//     </html>
-//   );
-// }
-import { PropsWithChildren } from 'react';
-import { Metadata } from 'next';
+import type { PropsWithChildren } from 'react';
+import type { Metadata } from 'next';
 import { getLocale } from 'next-intl/server';
-import { NextIntlClientProvider } from 'next-intl';
 
 import { Root } from '@/components/Root/Root';
 import { I18nProvider } from '@/core/i18n/provider';
@@ -49,19 +17,18 @@ export const metadata: Metadata = {
 export default async function RootLayout({ children }: PropsWithChildren) {
   const locale = await getLocale();
 
-  const messages = await import(`@/locales/${locale}.json`).then((module) => module.default);
-
   return (
     <html lang={locale}>
-      <body>
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          <I18nProvider>
-            <Root>
-              {children}
-            </Root>
-          </I18nProvider>
-        </NextIntlClientProvider>
-      </body>
+    <body>
+      <I18nProvider>
+        <Root>
+          {children}
+        </Root>
+      </I18nProvider>
+    </body>
     </html>
   );
 }
+
+
+
